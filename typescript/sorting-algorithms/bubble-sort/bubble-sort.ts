3bbbdf1d09e50595a324445d67de4391fd646a67
Taku Kobachi <>
2018-07-01 19:27:01 +0900

    Fix argument in function

--- typescript/sorting-algorithms/bubble-sort/bubble-sort.ts
+++ typescript/sorting-algorithms/bubble-sort/bubble-sort.ts
@@ -2,23 +2,23 @@
 const arrayRandom: number[] = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]
 
 const bubbleSortBasic = (array: number[]): number[] => {
-  array.forEach((value: number, i:number) => {
-    array.forEach((value: number, j:number) => {
-      if(array[j - 1] > array[j]) {
-        [array[j - 1], array[j]] = [array[j], array[j - 1]]
-      }
-    })
+  array.forEach(() => {
+	  array.forEach((j: number) => {
+		  if (array[j - 1] > array[j]) {
+			  [array[j - 1], array[j]] = [array[j], array[j - 1]];
+		  }
+	  });
   })
   return array
 }
-console.log(bubbleSortBasic(arrayRandom.slice())) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(bubbleSortBasic(arrayRandom)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
 
 const bubbleSort = (array: number[]): number[] => {
   let swapped: boolean
   do {
     swapped = false
-    array.forEach((value: number, i:number) => {
+    array.forEach((i:number) => {
       if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
         [array[i], array[i + 1]] = [array[i + 1], array[i]]
         swapped = true
@@ -27,4 +27,4 @@ const bubbleSort = (array: number[]): number[] => {
   } while(swapped)
   return array
 }
-console.log(bubbleSort(arrayRandom.slice())) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(bubbleSort(arrayRandom)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
