ec73a9ed9876ac726989e2c17bd5708c9514f970
Taku Kobachi <>
2018-08-05 20:48:41 +0900

    Fix insertion-sort

--- sorting-algorithms/insertion-sort/insertion-sort.ts
+++ sorting-algorithms/insertion-sort/insertion-sort.ts
@@ -1,17 +1,17 @@
 // array to sort
-const array: number[] = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]
+const arrayForInsertionsort: number[] = [10, 2, 5, 6, 4, 3, 7, 9, 1, 8]
 
 const insertionSort = (array: number[]): number[] => {
   array.forEach((_: number, i:number) => {
-    let temp: number = array[i]
+    let tmp: number = array[i]
     let j: number = i - 1
-    while (j >= 0 && array[j] > temp) {
+    while (j >= 0 && array[j] > tmp) {
       array[j + 1] = array[j]
       j--
     }
-    array[j + 1] = temp
+    array[j + 1] = tmp
   })
   return array
 }
 
-console.log(insertionSort(array)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(insertionSort(arrayForInsertionsort)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
