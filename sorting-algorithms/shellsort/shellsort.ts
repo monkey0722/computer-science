9116167cfa22275fcac84e27c2edeb480637f329
Taku Kobachi <>
2018-08-05 20:46:31 +0900

    Fix shellsort

--- sorting-algorithms/shellsort/shellsort.ts
+++ sorting-algorithms/shellsort/shellsort.ts
@@ -1,5 +1,5 @@
 // array to sort
-const array: number[] = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]
+const arrayForShellsort: number[] = [10, 2, 5, 6, 4, 3, 7, 9, 1, 8]
 
 // gaps
 const gaps: number[] = [701, 301, 132, 57, 23, 10, 4, 1]
@@ -8,16 +8,16 @@ const shellsort = (array: number[]): number[] => {
   for(let g = 0; g < gaps.length; g++) {
     const gap: number = gaps[g]
     for(let i: number = gap; i < array.length; i++) {
-      const temp: number = array[i]
+      const tmp: number = array[i]
       let last: number = i
-      for(let j: number = i; j >= gap && array[j - gap] > temp; j -= gap) {
+      for(let j: number = i; j >= gap && array[j - gap] > tmp; j -= gap) {
         array[j] = array[j - gap]
         last -= gap
       }
-      array[last] = temp
+      array[last] = tmp
     }
   }
   return array
 }
 
-console.log(shellsort(array)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(shellsort(arrayForShellsort)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
