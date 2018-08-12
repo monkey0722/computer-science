ea49418bfbdd2dd6813070d8a2db295fcc7db359
Taku Kobachi <>
2018-08-12 16:28:35 +0900

    Fix bubble-sort of sorting-algorithms

--- sorting-algorithms/bubble-sort/bubble-sort.ts
+++ sorting-algorithms/bubble-sort/bubble-sort.ts
@@ -1,30 +1,30 @@
 // array to sort
-const arrayRandom: number[] = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]
+const bubbleSortArray: number[] = [8, 2, 5, 6, 4, 3, 7, 10, 1, 9]
 
 const bubbleSortBasic = (array: number[]): number[] => {
-  array.forEach(() => {
-	  array.forEach((j: number) => {
-		  if (array[j - 1] > array[j]) {
-			  [array[j - 1], array[j]] = [array[j], array[j - 1]];
-		  }
-	  });
-  })
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if(array[j - 1] > array[j]) {
+        [array[j - 1], array[j]] = [array[j], array[j - 1]]
+      }
+    }
+  }
   return array
 }
-console.log(bubbleSortBasic(arrayRandom)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(bubbleSortBasic(bubbleSortArray)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
 
 const bubbleSort = (array: number[]): number[] => {
   let swapped: boolean
   do {
     swapped = false
-    array.forEach((_:number, i:number) => {
+    for (let i = 0; i < array.length; i++) {
       if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
         [array[i], array[i + 1]] = [array[i + 1], array[i]]
         swapped = true
       }
-    })
+    }
   } while(swapped)
   return array
 }
-console.log(bubbleSort(arrayRandom)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(bubbleSort(bubbleSortArray)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
