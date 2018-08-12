10e041a244e8cf888404b6dac17395e8cf8c8907
Taku Kobachi <>
2018-08-12 16:35:28 +0900

    Fix selection-sort-counters of sorting-algorithms

--- sorting-algorithms/selection-sort-counters/selection-sort-counters.ts
+++ sorting-algorithms/selection-sort-counters/selection-sort-counters.ts
@@ -1,24 +1,24 @@
 // sample of arrays to sort
 
-const arrayRandom: number[] = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]
-const arrayOrdered: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const arrayReversed: number[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+const arrayForSelectionSortCountersRandom: number[] = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]
+const arrayForSelectionSortCountersOrdered: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const arrayForSelectionSortCountersReversed: number[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 const selectionSort = (array: number[]): void => {
   let countOuter: number = 0
   let countInner: number = 0
   let countSwap: number = 0
 
-  for(let i: number = 0; i < array.length; i++) {
+  for (let i: number = 0; i < array.length; i++) {
     countOuter++
     let min: number = i
-    for(let j: number = i + 1; j < array.length; j++ ) {
+    for (let j: number = i + 1; j < array.length; j++ ) {
       countInner++
-      if(array[j] < array[min]) {
+      if (array[j] < array[min]) {
         min = j
       }
     }
-    if(i !== min) {
+    if (i !== min) {
       countSwap++
       [array[i], array[min]] = [array[min], array[i]]
     }
@@ -27,6 +27,6 @@ const selectionSort = (array: number[]): void => {
   console.log(`outer: ${countOuter}, inner: ${countInner}, swap: ${countSwap}`)
 }
 
-selectionSort(arrayRandom.slice()) // => outer: 10 inner: 45 swap: 5
-selectionSort(arrayOrdered.slice()) // => outer: 10 inner: 45 swap: 0
-selectionSort(arrayReversed.slice()) // => outer: 10 inner: 45 swap: 5
+selectionSort(arrayForSelectionSortCountersRandom.slice()) // => outer: 10 inner: 45 swap: 5
+selectionSort(arrayForSelectionSortCountersOrdered.slice()) // => outer: 10 inner: 45 swap: 0
+selectionSort(arrayForSelectionSortCountersReversed.slice()) // => outer: 10 inner: 45 swap: 5
