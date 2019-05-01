2cd394530b620f1841baa092c4d05764eaaafaa2
Taku Kobachi <>
2019-05-02 00:15:29 +0900

    add selection-sort.test.ts

---
+++ sorting-algorithms/selection-sort/selection-sort.test.ts
@@ -0,0 +1,10 @@
+import { selectionSort } from './selection-sort'
+
+describe('selection-sort', () => {
+  it('normal', () => {
+    // array for selection-sort
+    const array: number[] = [7, 3, 5, 6, 4, 2, 9, 10, 1, 8]
+    const result: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    expect(selectionSort(array)).toEqual(result)
+  })
+})
--- sorting-algorithms/selection-sort/selection-sort.ts
+++ sorting-algorithms/selection-sort/selection-sort.ts
@@ -1,7 +1,4 @@
-// array for selection-sort
-const array: number[] = [7, 3, 5, 6, 4, 2, 9, 10, 1, 8]
-
-const selectionSort = (array: number[]): number[] => {
+export const selectionSort = (array: number[]): number[] => {
   for (let i = 0; i < array.length; i++) {
     let min: number = i
     for (let j: number = i + 1; j < array.length; j++) {
@@ -15,5 +12,3 @@ const selectionSort = (array: number[]): number[] => {
   }
   return array
 }
-
-console.log(selectionSort(array)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
