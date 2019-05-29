c182e0494e3a0ced269afbbbd00168d4bbd390bd
Taku Kobachi <>
2019-05-29 20:45:27 +0900

    add insertion-sort.test.ts

---
+++ sorting-algorithms/insertion-sort/insertion-sort.test.ts
@@ -0,0 +1,10 @@
+import { insertionSort }  from './insertion-sort'
+
+describe('insertion-sort', () => {
+  it('normal', () => {
+    // array for insertion sort
+    const arrayForInsertionSort: number[] = [10, 2, 5, 6, 4, 3, 7, 9, 1, 8]
+    const result: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    expect(insertionSort(arrayForInsertionSort)).toEqual(result)
+  })
+})
--- sorting-algorithms/insertion-sort/insertion-sort.ts
+++ sorting-algorithms/insertion-sort/insertion-sort.ts
@@ -1,7 +1,4 @@
-// array for insertion sort
-const arrayForInsertionSort: number[] = [10, 2, 5, 6, 4, 3, 7, 9, 1, 8]
-
-const insertionSort = (array: number[]): number[] => {
+export const insertionSort = (array: number[]): number[] => {
   for (let i = 0; i < array.length; i++) {
     const tmp: number = array[i]
     let j: number = i - 1
@@ -13,5 +10,3 @@ const insertionSort = (array: number[]): number[] => {
   }
   return array
 }
-
-console.log(insertionSort(arrayForInsertionSort)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
