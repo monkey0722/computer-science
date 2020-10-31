a0dd4a926cff73e50232d8550c79c7076f6b0407
monkey0722 <ta19028@gmail.com>
2020-10-31 17:40:47 +0900

    Update quickSort

--- sorting-algorithms/quick-sort/quick-sort.test.ts
+++ sorting-algorithms/quick-sort/quick-sort.test.ts
@@ -2,7 +2,6 @@ import { quickSort } from "./quick-sort";
 
 describe("quickSort", () => {
   it("normal", () => {
-    // array for quick sort
     const arrayForQuickSort: number[] = [3, 2, 5, 6, 4, 9, 7, 10, 1, 8];
     const result: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     expect(quickSort(arrayForQuickSort)).toEqual(result);
