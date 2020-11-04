ab2c3634807f3a7a7829d52da5f049424055d13b
monkey0722 <ta19028@gmail.com>
2020-11-04 20:21:50 +0900

    Update insertionSortCounters

--- sorting-algorithms/insertion-sort-counters/insertion-sort-counters.ts
+++ sorting-algorithms/insertion-sort-counters/insertion-sort-counters.ts
@@ -1,63 +1,25 @@
-// array for insertion-sort-counters
-const insertionSortCountersArrayRandom: number[] = [
-  2,
-  8,
-  5,
-  6,
-  4,
-  3,
-  10,
-  7,
-  1,
-  9,
-];
-const insertionSortCountersArrayOrdered: number[] = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-];
-const insertionSortCountersArrayReversed: number[] = [
-  10,
-  9,
-  8,
-  7,
-  6,
-  5,
-  4,
-  3,
-  2,
-  1,
-];
+type Result = {
+  countOuter: number;
+  countInner: number;
+  countSwap: number;
+};
 
-const insertionSortCounters = (array: number[]): number[] => {
+export function insertionSortCounters<T>(items: Array<T>): Result {
   let countOuter: number = 0;
   let countInner: number = 0;
   let countSwap: number = 0;
 
-  for (let i: number = 0; i < array.length; i++) {
+  for (let i: number = 0; i < items.length; i++) {
     countOuter++;
-    const tmp: number = array[i];
+    const tmp: T = items[i];
     let j: number = i - 1;
-    while (j >= 0 && array[j] > tmp) {
+    while (j >= 0 && items[j] > tmp) {
       countInner++;
       countSwap++;
-      array[j + 1] = array[j];
+      items[j + 1] = items[j];
       j--;
     }
-    array[j + 1] = tmp;
+    items[j + 1] = tmp;
   }
-
-  console.log(`outer: ${countOuter}, inner: ${countInner}, swap: ${countSwap}`);
-  return array;
-};
-
-insertionSortCounters(insertionSortCountersArrayRandom); // => outer: 10 inner: 20 swap: 20
-insertionSortCounters(insertionSortCountersArrayOrdered); // => outer: 10 inner: 0 swap: 0
-insertionSortCounters(insertionSortCountersArrayReversed); // => outer: 10 inner: 45 swap: 45
+  return { countOuter, countInner, countSwap };
+}
