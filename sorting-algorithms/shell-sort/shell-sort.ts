b02aeb83a2d9b36273c600228c65cc77c9842a67
monkey0722 <ta19028@gmail.com>
2020-11-08 15:50:35 +0900

    Update shellSort

--- sorting-algorithms/shell-sort/shell-sort.ts
+++ sorting-algorithms/shell-sort/shell-sort.ts
@@ -1,18 +1,17 @@
-// gaps for shellsort
-const gapsForShellSort: number[] = [701, 301, 132, 57, 23, 10, 4, 1];
+const gaps: number[] = [701, 301, 132, 57, 23, 10, 4, 1];
 
-export const shellSort = (array: number[]): number[] => {
-  for (let g = 0; g < gapsForShellSort.length; g++) {
-    const gap: number = gapsForShellSort[g];
-    for (let i: number = gap; i < array.length; i++) {
-      const tmp: number = array[i];
+export function shellSort<T>(items: Array<T>): Array<T> {
+  for (let g = 0; g < gaps.length; g++) {
+    const gap: number = gaps[g];
+    for (let i = gap; i < items.length; i++) {
+      const tmp: T = items[i];
       let last: number = i;
-      for (let j: number = i; j >= gap && array[j - gap] > tmp; j -= gap) {
-        array[j] = array[j - gap];
+      for (let j = i; j >= gap && items[j - gap] > tmp; j -= gap) {
+        items[j] = items[j - gap];
         last -= gap;
       }
-      array[last] = tmp;
+      items[last] = tmp;
     }
   }
-  return array;
-};
+  return items;
+}
