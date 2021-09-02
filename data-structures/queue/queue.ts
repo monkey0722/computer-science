f8a4c91e4811e7798bd73cb2bd0d3a9a439a0cf2
monkey0722 <taku.kobachi@kanmu.co.jp>
2021-09-02 18:48:39 +0900

    feat(data-structures): Adapt hard private

--- data-structures/queue/queue.ts
+++ data-structures/queue/queue.ts
@@ -1,33 +1,33 @@
 import { Node, DummyHeadNode, DummyTailNode } from "../node";
 
 export class Queue<T> {
-  private _dummyHead: DummyHeadNode;
-  private _dummyTail: DummyTailNode;
-  private _length: number;
+  #dummyHead: DummyHeadNode;
+  #dummyTail: DummyTailNode;
+  #length: number;
 
   constructor() {
-    this._dummyHead = new DummyHeadNode();
-    this._dummyTail = new DummyTailNode();
-    this._dummyHead.next = this._dummyTail;
-    this._dummyTail.prev = this._dummyHead;
-    this._length = 0;
+    this.#dummyHead = new DummyHeadNode();
+    this.#dummyTail = new DummyTailNode();
+    this.#dummyHead.next = this.#dummyTail;
+    this.#dummyTail.prev = this.#dummyHead;
+    this.#length = 0;
   }
 
   isEmpty(): boolean {
-    return this._length === 0;
+    return this.#length === 0;
   }
 
   enqueue(value: T): number {
     const node = new Node(value);
-    const prevLast = this._dummyTail.prev as Node<T> | DummyHeadNode;
+    const prevLast = this.#dummyTail.prev as Node<T> | DummyHeadNode;
 
     prevLast.next = node;
     node.prev = prevLast;
-    node.next = this._dummyTail;
-    this._dummyTail.prev = node;
-    this._length++;
+    node.next = this.#dummyTail;
+    this.#dummyTail.prev = node;
+    this.#length++;
 
-    return this._length;
+    return this.#length;
   }
 
   dequeue(): T | undefined {
@@ -35,13 +35,13 @@ export class Queue<T> {
       return undefined;
     }
 
-    const node = this._dummyHead.next as Node<T>;
+    const node = this.#dummyHead.next as Node<T>;
     const newFirst = node.next as Node<T> | DummyTailNode;
 
-    this._dummyHead.next = newFirst;
-    newFirst.prev = this._dummyHead;
+    this.#dummyHead.next = newFirst;
+    newFirst.prev = this.#dummyHead;
     node.next = null;
-    this._length--;
+    this.#length--;
 
     return node.value;
   }
@@ -50,19 +50,17 @@ export class Queue<T> {
     if (this.isEmpty()) {
       return undefined;
     }
-
-    return (this._dummyHead.next as Node<T>).value;
+    return (this.#dummyHead.next as Node<T>).value;
   }
 
   back(): T | undefined {
     if (this.isEmpty()) {
       return undefined;
     }
-
-    return (this._dummyTail.prev as Node<T>).value;
+    return (this.#dummyTail.prev as Node<T>).value;
   }
 
   get length(): number {
-    return this._length;
+    return this.#length;
   }
 }
