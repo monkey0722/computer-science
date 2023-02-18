dfe4fe291026c7ea811b1c72899c78d1d00c1194
monkey0722 <monkeys7222@gmail.com>
2023-02-18 13:18:00 +0900

    feat(math): Add primeFactors and sieveOfEratosthenes

---
+++ math/primeFactors/primeFactors.test.ts
@@ -0,0 +1,23 @@
+import {primeFactors} from './primeFactors';
+
+describe('primeFactors', () => {
+  test('should return an empty array when passed 1', () => {
+    expect(primeFactors(1)).toEqual([]);
+  });
+
+  test('should return an array containing 2 and 2 when passed 4', () => {
+    expect(primeFactors(4)).toEqual([2, 2]);
+  });
+
+  test('should return an array containing 2, 3, and 5 when passed 30', () => {
+    expect(primeFactors(30)).toEqual([2, 3, 5]);
+  });
+
+  test('should return an array containing 2, 2, 2, and 3 when passed 24', () => {
+    expect(primeFactors(24)).toEqual([2, 2, 2, 3]);
+  });
+
+  test('should return an array containing 2 and 7 when passed 14', () => {
+    expect(primeFactors(14)).toEqual([2, 7]);
+  });
+});
---
+++ math/sieveOfEratosthenes/sieveOfEratosthenes.ts
@@ -0,0 +1,28 @@
+/**
+ * Use the Sieve of Eratosthenes algorithm to generate a list of primes up to n.
+ * @param {number} n - The upper limit for prime numbers.
+ * @returns {Array<number>} An array of prime numbers up to n.
+ */
+export function sieveOfEratosthenes(n: number): Array<number> {
+  // Create an array containing all integers from 2 to n
+  let primes = new Array(n + 1);
+  primes.fill(true);
+
+  // Mark all multiples of each prime as composite
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (primes[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        primes[j] = false;
+      }
+    }
+  }
+
+  // Collect all remaining prime numbers
+  let result: Array<number> = [];
+  for (let i = 2; i <= n; i++) {
+    if (primes[i]) {
+      result.push(i);
+    }
+  }
+  return result;
+}
