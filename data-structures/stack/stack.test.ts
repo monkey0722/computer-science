572dfbaaf1d334b489c22c8e5b104d4d3092ccdf
monkey0722 <monkeys7222@gmail.com>
2023-11-08 19:14:39 +0900

    refactor(data-structures): Update 'Stack'

--- data-structures/stack/stack.test.ts
+++ data-structures/stack/stack.test.ts
@@ -1,95 +1,85 @@
 import {Stack} from './stack';
 
 describe('Stack', () => {
-  test('constructor initializes an empty stack', () => {
-    const stack = new Stack<number>();
-    expect(stack).toBeTruthy();
-    expect(stack.size).toBe(0);
+  let stack: Stack<number>;
+
+  beforeEach(() => {
+    stack = new Stack<number>();
   });
 
-  test('length is maintained correctly', () => {
-    const stack = new Stack<number>();
-    expect(stack.size).toBe(0);
-    stack.push(100);
-    expect(stack.size).toBe(1);
-    stack.push(200);
-    expect(stack.size).toBe(2);
-    stack.pop();
-    expect(stack.size).toBe(1);
-    stack.push(300);
-    expect(stack.size).toBe(2);
-  });
-
-  test('push adds elements to the stack', () => {
-    const stack = new Stack<number>();
-    stack.push(100);
-    expect(stack.peek()).toBe(100);
-    stack.push(200);
-    expect(stack.peek()).toBe(200);
-    expect(stack.size).toBe(2);
-  });
-
-  test('pop returns and removes the last element', () => {
-    const stack = new Stack<number>();
-    stack.push(100);
-    stack.push(200);
-    expect(stack.pop()).toBe(200);
-    expect(stack.size).toBe(1);
-    expect(stack.pop()).toBe(100);
-    expect(stack.size).toBe(0);
-    expect(stack.pop()).toBe(undefined);
-  });
-
-  test('pop works correctly with objects', () => {
-    const stack = new Stack<object>();
-    const obj1 = { a: 1 };
-    const obj2 = { b: 2 };
-    stack.push(obj1);
-    stack.push(obj2);
-    expect(stack.pop()).toBe(obj2);
-    expect(stack.pop()).toBe(obj1);
-  });
-
-  test('isEmpty indicates if the stack is empty', () => {
-    const stack = new Stack<number>();
+  test('new stack should be empty', () => {
     expect(stack.isEmpty()).toBe(true);
-    stack.push(100);
+  });
+
+  test('push should add an item to the stack', () => {
+    stack.push(1);
+    expect(stack.size()).toBe(1);
+  });
+
+  test('push should add items in LIFO order', () => {
+    stack.push(1);
+    stack.push(2);
+    expect(stack.peek()).toBe(2);
+  });
+
+  test('pop should remove the last item', () => {
+    stack.push(1);
+    stack.push(2);
+    const item = stack.pop();
+    expect(item).toBe(2);
+    expect(stack.size()).toBe(1);
+  });
+
+  test('pop should return undefined for an empty stack', () => {
+    expect(stack.pop()).toBeUndefined();
+  });
+
+  test('peek should return the last item without removing it', () => {
+    stack.push(1);
+    expect(stack.peek()).toBe(1);
+    expect(stack.size()).toBe(1);
+  });
+
+  test('size should return the number of items in the stack', () => {
+    stack.push(1);
+    stack.push(2);
+    expect(stack.size()).toBe(2);
+  });
+
+  test('isEmpty should return false for stack with items', () => {
+    stack.push(1);
     expect(stack.isEmpty()).toBe(false);
-    stack.pop();
+  });
+
+  test('isFull should return true when stack reaches capacity', () => {
+    const limitedStack = new Stack<number>(2);
+    limitedStack.push(1);
+    limitedStack.push(2);
+    expect(limitedStack.isFull()).toBe(true);
+  });
+
+  test('push should throw an error when trying to add items beyond capacity', () => {
+    const limitedStack = new Stack<number>(1);
+    limitedStack.push(1);
+    expect(() => {
+      limitedStack.push(2);
+    }).toThrow('Stack overflow');
+  });
+
+  test('clear should remove all items from the stack', () => {
+    stack.push(1);
+    stack.push(2);
+    stack.clear();
     expect(stack.isEmpty()).toBe(true);
   });
 
-  test('peek returns the last element without removing it', () => {
-    const stack = new Stack<number>();
-    expect(stack.peek()).toBe(undefined);
-    stack.push(100);
-    expect(stack.peek()).toBe(100);
-    stack.push(200);
-    expect(stack.peek()).toBe(200);
-    stack.pop();
-    expect(stack.peek()).toBe(100);
-  });
-
-  test('handles large amounts of data', () => {
-    const stack = new Stack<number>();
-    const n = 10000;
-    for (let i = 0; i < n; i++) {
+  test('push performance for 10000 items', () => {
+    const start = performance.now();
+    for (let i = 0; i < 10000; i++) {
       stack.push(i);
     }
-    expect(stack.size).toBe(n);
-    for (let i = n - 1; i >= 0; i--) {
-      expect(stack.pop()).toBe(i);
-    }
-    expect(stack.isEmpty()).toBe(true);
-  });
-
-  test('stack handles different types when using generics', () => {
-    const numberStack = new Stack<number>();
-    numberStack.push(1);
-    expect(numberStack.peek()).toBe(1);
-
-    const stringStack = new Stack<string>();
-    stringStack.push('test');
-    expect(stringStack.peek()).toBe('test');
+    const end = performance.now();
+    console.log(`Time taken to push 10000 items: ${end - start}ms`);
+    expect(end - start).toBeLessThan(10);
   });
 });
--- data-structures/stack/stack.ts
+++ data-structures/stack/stack.ts
@@ -1,38 +1,68 @@
-import {Node} from '../node';
-
-type Tail<T> = Node<T> | null
-
+/**
+ * Class representing a stack data structure.
+ * @template T - The type of elements held in the stack.
+ */
 export class Stack<T> {
-  private tail: Tail<T> = null;
-  private length: number = 0;
+  private storage: T[] = [];
 
-  isEmpty(): boolean {
-    return this.length === 0;
-  }
+  constructor(private capacity: number = Infinity) {}
 
-  push(value: T): number {
-    const node = new Node(value);
-    node.next = this.tail;
-    this.tail = node;
-    return ++this.length;
-  }
-
-  pop(): T | undefined {
-    const node = this.tail;
-    if (!node) {
-      return undefined;
+  /**
+   * Adds an element to the top of the stack.
+   * @param {T} item - The item to be added to the stack.
+   * @throws {Error} if the stack has reached its capacity limit.
+   */
+  push(item: T): void {
+    if (this.size() === this.capacity) {
+      throw new Error('Stack overflow');
     }
-    this.tail = node.next as Tail<T>;
-    node.next = null;
-    this.length--;
-    return node.value;
+    this.storage.push(item);
   }
 
+  /**
+   * Removes the element from the top of the stack and returns it.
+   * @return {T | undefined} The element at the top of the stack or undefined if the stack is empty.
+   */
+  pop(): T | undefined {
+    return this.storage.pop();
+  }
+
+  /**
+   * Returns the element at the top of the stack without removing it.
+   * @return {T | undefined} The element at the top of the stack or undefined if the stack is empty.
+   */
   peek(): T | undefined {
-    return this.tail?.value;
+    return this.storage[this.size() - 1];
   }
 
-  get size(): number {
-    return this.length;
+  /**
+   * Returns the number of elements in the stack.
+   * @return {number} The size of the stack.
+   */
+  size(): number {
+    return this.storage.length;
+  }
+
+  /**
+   * Checks if the stack is empty.
+   * @return {boolean} True if the stack is empty, false otherwise.
+   */
+  isEmpty(): boolean {
+    return this.size() === 0;
+  }
+
+  /**
+   * Checks if the stack has reached its capacity.
+   * @return {boolean} True if the stack is full, false otherwise.
+   */
+  isFull(): boolean {
+    return this.size() === this.capacity;
+  }
+
+  /**
+   * Empties the stack of all elements.
+   */
+  clear(): void {
+    this.storage = [];
   }
 }
